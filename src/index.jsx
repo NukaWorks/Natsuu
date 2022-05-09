2e53e9f929f79ae7fe516a688da575fba46456c8
Powerm1nt <nukaworks@nuka.works>
2022-05-09 20:14:25 +0200

    Project Update

---
+++ src/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+    <React.Fragment>
+        <h1>Hello World !</h1>
+    </React.Fragment>,
+    document.getElementById("root")
+);
--- src/types/notecards.tsx
+++
@@ -1,9 +0,0 @@
-type NotecardType =
-  | "success"
-  | "warning"
-  | "deprecated"
-  | "error"
-  | "negative"
-  | "info";
-
-export type { NotecardType };
